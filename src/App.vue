7b6fb1d1d984af3385b1a77fa072301f74f5838e
carlos <carlos.hrq.rodrigues@gmail.com>
2020-07-19 16:24:31 -0300

    contador

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+
+        <h1 class="display-4">Vuex</h1>
+        <p class="lead">Padrão de Gerenciamento de estado centralizado no Vue.</p>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <Contador />
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Contador from '@/components/Contador'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Contador
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2 class="font-weight-light">Contador {{contador}}</h2>
+        <button 
+            class="btn btn-lg btn-danger mr-1"
+            @click="decrementar">
+                -
+        </button>
+        <button 
+            class="btn btn-lg btn-success"
+            @click="incrementar">
+                +
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0,
+        }
+    },
+    methods: {
+        decrementar() {
+            this.contador--;
+        },
+        incrementar() {
+            this.contador++;
+        }
+    }
+}
+</script>
